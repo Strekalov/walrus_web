e6983308c3e4520f259719916fb74943289575ab
Стрекалов Артем <strekalov0793@yandex.ru>
2022-05-29 04:42:24 +0500

    Добавлены отчёты.

--- main.py
+++ main.py
@@ -1,43 +1,57 @@
 import streamlit as st
 from walrus_web.rendering import render_title, render_draw_select
-from walrus_web.utils import bytes_to_numpy, draw_boxes
+from walrus_web.utils import (
+    bytes_to_numpy,
+    draw_boxes,
+    boxes_to_points,
+    make_csv_report_coords,
+    make_csv_report_count,
+)
 import numpy as np
 import cv2
 import time
 import base64
+import os
 
 result_title = st.empty()
 
 
 def get_walrus_boxes(image):
     "заменить на реальную Йоло!"
-    return [{"box": [200, 200, 200, 200]},{"box": [400, 600, 500, 500]}]
-    
-    
+    return [{"box": [200, 200, 200, 200]}, {"box": [400, 600, 500, 500]}]
+
+
 @st.cache
 def detect_object(image: np.ndarray):
-    walrus_count = 10
     yolo_result = get_walrus_boxes(image)
-    boxes = [el['box'] for el in yolo_result]
-    walrus_count = len(boxes)
+    boxes = [el["box"] for el in yolo_result]
+    result = {}
+
+    result["points"] = boxes_to_points(boxes)
+    result["boxes"] = boxes
+    result["walruses_count"] = len(boxes)
+
     time.sleep(2)
-    return boxes, walrus_count
+    return result
 
 
 def count_walruses(uploaded_images: list):
     my_bar = st.progress(0)
-    
+
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.write(' ')
+        st.write(" ")
     with col2:
         proccess_image = st.empty()
     with col3:
-        st.write(' ')
-    
+        st.write(" ")
+
     count_images = len(uploaded_images)
-    
+
     all_walruses_count = 0
+    coords_report_data = []
+    all_walruses_data = []
+
     if count_images > 0:
         with col2:
             with open("assets/walrus_progress.gif", "rb") as file:
@@ -47,59 +61,74 @@ def count_walruses(uploaded_images: list):
                     f'<img src="data:image/gif;base64,{data_url}">',
                     unsafe_allow_html=True,
                 )
-                        
-            
+
     with st.expander("Разверните для просмотра деталей"):
         for i, image in enumerate(uploaded_images):
-
+            filename, extension = os.path.splitext(image.name)
             opencv_image = bytes_to_numpy(image)
-            
-            if isinstance(opencv_image , np.ndarray):
-                with st.spinner(f'Считаю моржей на {i+1} изображении ...'):
-                    
-                    boxes, valrus_count = detect_object(opencv_image)
+
+            if isinstance(opencv_image, np.ndarray):
+                with st.spinner(f"Считаю моржей на {i+1} изображении ..."):
+
+                    walruses_data = detect_object(opencv_image)
+                    walruses_data["filename"] = filename
+                    all_walruses_data.append(walruses_data)
                     image_with_boxes = opencv_image.copy()
-                    for box in boxes:
+                    walrus_count = walruses_data["walruses_count"]
+
+                    for box in walruses_data["boxes"]:
                         draw_boxes(image_with_boxes, box, draw_variant=draw_variant)
-                    all_walruses_count += valrus_count
-                st.image(image_with_boxes,channels='BGR', caption=f"На изображении найдено{valrus_count} моржей!")
+                    all_walruses_count += walrus_count
+                st.image(
+                    image_with_boxes,
+                    channels="BGR",
+                    caption=f"На изображении найдено {walrus_count} моржей!",
+                )
 
             else:
                 st.error("С изображением что-то не так, проверьте и загрузите заново!")
-            
-            my_bar.progress((i+1)/count_images)
-            
+
+            my_bar.progress((i + 1) / count_images)
+
         if count_images > 0:
             with col1:
-                st.write(' ')
+                st.write(" ")
             with col2:
                 with open("assets/walrus_title.svg", "r") as file:
                     svg = file.read()
                     proccess_image.empty()
-                    proccess_image = st.image(svg, width=128, use_column_width='always')
-                    
+                    proccess_image = st.image(svg, width=128, use_column_width="always")
+
             with col3:
-                st.write(' ')
+                st.write(" ")
     if all_walruses_count > 0:
-        result_title = st.success(f"На загруженных изображениях найдено {all_walruses_count} моржей!")
+        result_title = st.success(
+            f"На загруженных изображениях найдено {all_walruses_count} моржей!"
+        )
+        make_csv_report_count(all_walruses_data)
+        make_csv_report_coords(all_walruses_data)
+
 
 render_title()
 draw_variant = render_draw_select()
 
+
 def main():
     # st.balloons()
-    
+
     imges_list = upload_image_ui()
     count_walruses(imges_list)
-    
+
 
 def upload_image_ui():
-    uploaded_images = st.file_uploader("Пожалуйста, выберите файлы с изображениями:", 
-                                      type=["png", "jpg", "jpeg"],
-                                      accept_multiple_files=True,
-                                      help="Нажмите, чтобы загрузить фото с моржами")
+    uploaded_images = st.file_uploader(
+        "Пожалуйста, выберите файлы с изображениями:",
+        type=["png", "jpg", "jpeg"],
+        accept_multiple_files=True,
+        help="Нажмите, чтобы загрузить фото с моржами",
+    )
     return uploaded_images
-    
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
--- walrus_web/rendering.py
+++ walrus_web/rendering.py
@@ -1,32 +1,34 @@
 import streamlit as st
 
 
-def render_svg(svg_path:str):
+def render_svg(svg_path: str):
     """Renders the given svg string."""
     with open(svg_path, "r") as file:
         svg = file.read()
-    st.image(svg, width=128, use_column_width='always')
-    
+    st.image(svg, width=128, use_column_width="always")
+
 
 def render_element_in_center(st_element):
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.write(' ')
+        st.write(" ")
     with col2:
         st_element
     with col3:
-        st.write(' ')
-    
+        st.write(" ")
+
+
 def render_title():
-    
+
     st.title("Подсчёт моржей на изображении!")
-    
-    
+
+
 def render_draw_select():
     with open("assets/walrus_sidebar.svg", "r") as file:
         svg = file.read()
-    st.sidebar.image(svg, width=128, use_column_width='always')
-    draw_variant = st.sidebar.radio("Выбери как выделить моржей:",
-                     ('Никак', 'Рамки', 'Точки'))
-    
+    st.sidebar.image(svg, width=128, use_column_width="always")
+    draw_variant = st.sidebar.radio(
+        "Выбери как выделить моржей:", ("Никак", "Рамки", "Точки")
+    )
+
     return draw_variant
